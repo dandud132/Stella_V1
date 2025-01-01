23b143a343e153062e498210e806d89ce5391dc0
Danil <@>
2025-01-01 13:08:16 +0300

    добавлены  новые файлы

--- skills.py
+++ skills.py
@@ -20,7 +20,7 @@ def browser():
 # Функция для выключения компьютера
 def offpc():
     play_audio('base_answers/shutting_down_computer.mp3')
-    os.system('shutdown \s')
+    os.system('shutdown /s')
 
 # Функция для отключения бота
 def offBot():
